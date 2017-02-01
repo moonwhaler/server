875dd1728c65de007480e3fa783ab72cc35330e1
Chris <moonwhaler@users.noreply.github.com>
2017-02-01 18:30:05 +0100

    Update mangosd.sh
    Corrected spelling.

--- src/tools/restart-scripts/mangosd.sh
+++ src/tools/restart-scripts/mangosd.sh
@@ -3,7 +3,7 @@
 #Change the path variable that matches your own
 #Do not add a trailing slash
 path="/home/mangos/server/bin"
-SESSION="mangos"
+SESSION="mangosd"
 DAEMON="screen -d -m -S $SESSION $path/mangosd_check.sh"
 screen -r $SESSION -ls -q 2>&1 >/dev/null
 echo -e ""
