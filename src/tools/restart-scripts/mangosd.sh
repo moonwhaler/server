b5c1a139f50506a6b89cf30c430fe29ea14bf411
Chris <moonwhaler@users.noreply.github.com>
2017-02-01 18:23:20 +0100

    Update and rename mangos.sh to mangosd.sh
    Corrected file names accordingly.

--- src/tools/restart-scripts/mangosd.sh
+++ src/tools/restart-scripts/mangosd.sh
@@ -4,7 +4,7 @@
 #Do not add a trailing slash
 path="/home/mangos/server/bin"
 SESSION="mangos"
-DAEMON="screen -d -m -S $SESSION $path/mangos_check.sh"
+DAEMON="screen -d -m -S $SESSION $path/mangosd_check.sh"
 screen -r $SESSION -ls -q 2>&1 >/dev/null
 echo -e ""
 echo "Mangos World has been launched into the background."
